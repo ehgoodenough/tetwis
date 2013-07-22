dd51fd89d3734bd451fb49b44db0f8491b249d98
Arcym <andrewmcp333@gmail.com>
2013-07-22 18:10:23 -0400

    Defined the components for instantiating a window.

--- Tetwis.java
+++ Tetwis.java
@@ -1,7 +1,40 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Tetwis
 {
 	public static void main(String[] args)
 	{
-		//tetwis!!
+		new JFrame()
+		{
+			{
+				setTitle("Tetwis");
+				setSize(320, 320);
+				setResizable(false);
+				
+				add(new TetwisJComponent()); pack();
+				
+				setLocationRelativeTo(null);
+				setDefaultCloseOperation(EXIT_ON_CLOSE);
+				setVisible(true);
+			}
+		};
+	}
+	
+	public static class TetwisJComponent extends JComponent
+	{
+		public void paintComponent(Graphics GFX)
+		{
+			Graphics2D GFX2D = (Graphics2D)GFX;
+			
+			Shape background = new Rectangle(getPreferredSize());
+			GFX2D.setColor(Color.BLUE); GFX2D.fill(background);
+			GFX2D.draw(background);
+		}
+		
+		public Dimension getPreferredSize()
+		{
+			return new Dimension(360, 480);
+		}
 	}
 }
