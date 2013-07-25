3fa07af1e88c6127823f13af8fed51dc142ddfad
Arcym <andrewmcp333@gmail.com>
2013-07-25 08:23:13 -0400

    Removed the temporary tetribits in the tetratrix.

--- Tetratrix.java
+++ Tetratrix.java
@@ -3,12 +3,6 @@ public class Tetratrix
 	public final int WIDTH_IN_TETRIBITS = 10; public final int HEIGHT_IN_TETRIBITS = 18;
 	public Tetribit[][] tetribits = new Tetribit[WIDTH_IN_TETRIBITS][HEIGHT_IN_TETRIBITS];
 	
-	public Tetratrix()
-	{
-		tetribits[0][0] = new Tetribit(0, 255, 0);
-		tetribits[9][17] = new Tetribit(0, 255, 0);
-	}
-	
 	public boolean canDrop(Tetromino tetromino)
 	{
 		for(int x = 0; x < tetromino.tetribits.length; x++)
