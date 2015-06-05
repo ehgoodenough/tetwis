37e1963f5bea7725a257ce4ccbd07a7fa4f9a4ee
Andrew McPherson <andrewmcp333@gmail.com>
2015-06-05 13:08:13 -0400

    Fixed the indentation of all the code.

--- Tetribit.java
+++ Tetribit.java
@@ -2,10 +2,10 @@ import java.awt.Color;
 
 public class Tetribit
 {
-	public Color color;
-	
-	public Tetribit(Color color)
-	{
-		this.color = color;
-	}
+    public Color color;
+    
+    public Tetribit(Color color)
+    {
+        this.color = color;
+    }
 }
